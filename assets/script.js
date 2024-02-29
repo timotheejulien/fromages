83d1285bf2d0058296ce61fd731f6d8eb00e03aa
Timothée <tim.julien@icloud.com>
2024-02-29 13:10:04 +0100

    Ajout : fonctionnalité de recherche d'un post

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ function mailTo() {
 	var $mailToSubject = "Notes caséologiques : prise de contact";
 	window.location.href='mailto:'+$mailToEmail+'?subject='+$mailToSubject;
 }
+
+function search_modal() {
+	el = document.getElementById("search_modal");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+	document.getElementById("search-input").focus();
+}
