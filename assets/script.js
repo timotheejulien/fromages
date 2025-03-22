7ca82d19a4fc0af29d05ba2ad233156adb78d3ee
Timothée <tim.julien@icloud.com>
2025-03-22 10:44:44 +0100

    Design & bug fix: change font parameter
    - Design: change font parameter

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,13 @@ function search_modal() {
 	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 	document.getElementById("search-input").focus();
 }
+
+document.addEventListener("keydown", function(event) {  
+   if (event.key === "Escape") {  
+       let input = document.getElementById("search-input");  
+       if (input) {
+           input.value = ""; // Efface le contenu de l'input
+       }
+       search_modal();  
+   }  
+});
